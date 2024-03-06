9d4c14f9650a97299205b4ec62393e439fe2d299
Philipp-Tanrek <145840230+Philipp-Tanrek@users.noreply.github.com>
2024-03-06 14:17:24 +0100

    Upload Files that create a csv out of the changed Probes Beta values
    Upload Files that create a csv out of the changed Probes Beta values

---
+++ Create CSV of Beta Values form changed Probes (EPICv1).r
@@ -0,0 +1,75 @@
+#load librarys
+library(sesame)
+library(tidyverse)
+
+# Initialize two dfs for saving the beta values of the Type I and Type II probes
+df <- data.frame()
+df2 <- data.frame()
+
+#Load csv of indexes into a vector 
+EPICv1.Indexes<- read.csv("Index EPIC v1 changed probes.csv")
+EPICv1.Indexes <- EPICv1.Indexes$x
+
+# List all directories of EPIC v1 Samples
+List <- list.dirs("/home/minknow/EPIC_V1/", full.names = TRUE)
+#remove first entry because ist the Parent folder and it would load it all twice 
+List <- List[-1]
+
+#create a range  over all 16 samples
+range <- 1:16
+# Loop through each directory
+for (i in range) {
+    # Get the name of the folder + names of the file 
+    String <- paste(List[i],list.files(List[i])[1], sep = "/")
+    #remove the _Grn.idat /_Red.idat to be able to use readIDATpair
+    smaller_string <- substr(String, 1, nchar(String)-9)
+    #just checkin progress and that its loading the right sample
+    print(smaller_string)
+    #read sample using sesame into an sdf
+    sdf <- readIDATpair(smaller_string)
+    #get Probes that changed by Index
+    sdf <- sdf[c(EPICv1.Indexes),]
+    #subset by type to save them seperatly 
+    type1<- subset(sdf, col !=2)
+    type2<- subset(sdf, col ==2)
+    #orderby Probe_ID so the probes are always in the same order
+    #makes it easier to make scatteplots to see correlations
+    type1 <- type1[order(type1$Probe_ID),]
+    type2 <- type2[order(type2$Probe_ID),]
+    #get the Beta Values
+    betas1 <- getBetas(type1)
+    betas2 <- getBetas(type2)
+    # Create a data frame from the read data
+    frame1 <- data.frame(betas1)
+    frame2 <- data.frame(betas2)
+    #if its the first itteration of the loop initialize the df
+    #else just add a coulumn with the beta values 
+    if (i == 1) {
+        df <- frame1
+        df2 <- frame2
+    } else {
+        # add beta values as a column
+        df <- cbind(df, frame1)
+        df2 <- cbind(df2, frame2)
+    }
+}
+
+#create empty list 
+result_list <- vector("list", length = length(List))
+
+# Loop over each element in the List
+for (i in seq_along(List)) {
+  # Get the names of the Samples form the Folders and save them in a list
+  result_list[[i]] <- substr(List[i], 30, nchar(List[i])-8)
+}
+
+#Names the columns with the according Sample Name 
+names(df) <- result_list
+names(df2) <- result_list
+
+# quick check of the dfs 
+head(df)
+head(df2)
+#save the dfs as a csv to later plot 
+write.csv(df, "Changed Type I Probes EPIC v1.csv") 
+write.csv(df2, "Changed Type II Probes EPIC v1.csv")
